f0af951f3c206a2b8da8f36e029a45acedd76da7
maxime.materno@gmail.com <maxime.materno@gmail.com>
2019-10-04 11:13:06 -0700

    added unit tests

--- acme_test.py
+++ acme_test.py
@@ -2,17 +2,22 @@ import unittest
 from acme import Product
 from acme_report import generate_products, ADJECTIVES, NOUNS
 
-
 class AcmeProductTests(unittest.TestCase):
     """Making sure Acme products are the tops!"""
-
     def test_default_product_price(self):
         """Test default product price being 10."""
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
-
-
 ​
+    def test_default_product_weight(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.weight, 20)
+
+    def test_default_product_flam(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.flammability, 0.5)
 ​
 if __name__ == '__main__':
     unittest.main()
